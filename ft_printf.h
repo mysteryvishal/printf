4c23dda848dfd6db34964db1f934ab415fdd0c48
Vishal Mistry <vmistry@student.42london.com>
2026-01-16 13:57:08 +0000

    feat: add ft_printf header and implementation for strings and chars

---
+++ ft_printf.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vmistry <vmistry@student.42london.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/30 05:06:22 by vmistry           #+#    #+#             */
+/*   Updated: 2026/01/16 13:40:12 by vmistry          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PRINTF_H
+# define PRINTF_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdarg.h>
+# include "./libft.h"
+
+int	ft_printf(const char *, ...);
+#endif
