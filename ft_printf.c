4b0e7a10ea6d3f89004c563021ef4b5c876ef993
Vishal Mistry <vmistry@c1r7s3.42london.com>
2026-01-16 16:54:07 +0000

    fix: update handle_type function to use pf functions to handle character counts

--- ft_printf.c
+++ ft_printf.c
@@ -6,21 +6,26 @@
 /*   By: vmistry <vmistry@student.42london.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/15 13:07:04 by vmistry           #+#    #+#             */
-/*   Updated: 2026/01/16 13:55:42 by vmistry          ###   ########.fr       */
+/*   Updated: 2026/01/16 16:51:37 by vmistry          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "printf.h"
-#include "./libft.h"
 
-static int	handle_type(const char type, va_list args)
+static int	handle_type(const char type, va_list args, size_t *count)
 {
 	if (type == 'd' || type == 'i')
-		ft_putnbr_fd(va_arg(args, int), 1);
+		pf_putnbr(va_arg(args, int), 1);
 	else if (type == 'c' || type == '%')
-		ft_putchar_fd(va_arg(args, char), 1);
+		pf_putchar(va_arg(args, char), count);
 	else if (type == 's')
-		ft_putstr_fd(va_arg(args, char *), 1);
+		pf_putstr(va_arg(args, char *), count);
+	else if (type == 'u')
+		pf_putunbr(va_arg(args, unsigned int), count);
+	else if (type == 'x' || type == 'X')
+		pf_puthex(va_arg(args, unsigned int), count);
+	else if (type == 'p')
+		pf_puthex(va_arg(args, unsigned int), count);
 	else
 		return (1);
 	return (0);
