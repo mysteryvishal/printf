ff0c0c0324f82786dc014a57406e05924b7a04ce
vmistry <vmistry@student.42london.com>
2026-01-17 13:31:29 +0000

    fix: update function calls in handler to new naming convention

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: vmistry <vmistry@student.42london.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/15 13:07:04 by vmistry           #+#    #+#             */
-/*   Updated: 2026/01/16 20:29:46 by vmistry          ###   ########.fr       */
+/*   Updated: 2026/01/17 13:28:46 by vmistry          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,19 @@
 static int	handle_type(const char type, va_list args, int *count)
 {
 	if (type == 'd' || type == 'i')
-		pf_putnbr(va_arg(args, int), count);
-	else if (type == 'c' || type == '%')
-		pf_putchar(va_arg(args, int), count);
+		pf_putid(va_arg(args, int), count);
+	else if (type == 'c')
+		pf_putc(va_arg(args, int), count);
+	else if (type == '%')
+		pf_putc('%', count);
 	else if (type == 's')
-		pf_putstr(va_arg(args, char *), count);
+		pf_puts(va_arg(args, char *), count);
 	else if (type == 'u')
-		pf_putunbr(va_arg(args, unsigned int), count);
+		pf_putu(va_arg(args, unsigned int), count);
 	else if (type == 'x' || type == 'X')
-		pf_puthex(va_arg(args, unsigned long long), count);
+		pf_puth(va_arg(args, unsigned long long), count, (type == 'X'));
 	else if (type == 'p')
-		pf_putptr(args, count);
-		// pf_putptr((unsigned long long) va_arg(args, void *), count);
+		pf_putp(va_arg(args, unsigned long long), count);
 	else
 		return (1);
 	return (0);
@@ -37,6 +38,7 @@ int	ft_printf(const char *format, ...)
 	va_list	args;
 	int	count;
 
+	count = 0;
 	if (format == NULL)
 		return (0);
 	va_start(args, format);
@@ -49,7 +51,7 @@ int	ft_printf(const char *format, ...)
 				return (1);
 		}
 		else
-			pf_putchar(*format, &count);
+			pf_putc(*format, &count);
 		format++;
 	}
 	va_end(args);
