3ebd1ac0054e21ce925a68e6b9fe2162d27d64ff
vmistry <vmistry@student.42london.com>
2026-01-17 15:17:49 +0000

    fix: handle null strings in pf_puts with "(null)"

--- ft_putcs.c
+++ ft_putcs.c
@@ -6,7 +6,7 @@
 /*   By: vmistry <vmistry@student.42london.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/17 12:59:26 by vmistry           #+#    #+#             */
-/*   Updated: 2026/01/17 13:42:51 by vmistry          ###   ########.fr       */
+/*   Updated: 2026/01/17 15:06:23 by vmistry          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ void	pf_putc(int c, int *count)
 void	pf_puts(char *s, int *count)
 {
 	if (!s)
-		return ;
+		return (pf_puts("(null)", count));
 	while (*s)
 	{
 		pf_putc(*s, count);
